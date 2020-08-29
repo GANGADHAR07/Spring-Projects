18124642dd13f5f62827c045c4e8ae050625f5d2
kgangadhar2 <kgangadhar2@IN-5CG0251NN1>
2020-08-29 15:12:54 +0530

    Spring JPA Commit 1

---
+++ SpringJpa/src/main/Java/com/dxc/repository/ProductRepository.java
@@ -0,0 +1,10 @@
+package com.dxc.repository;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.dxc.beans.Product;
+
+public interface ProductRepository extends JpaRepository<Product, Integer> {
+
+	Product findOne(int id);
+
+}
